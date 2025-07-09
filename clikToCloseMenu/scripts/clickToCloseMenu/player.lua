adedc5bde18d71ab3ab34f87cb3e5745be78bdbe
Drasenix <adowns@hotmail.fr>
2025-07-09 15:14:25 +0200

    Ignoring Dialogue Mode to avoid quest breaking and not going  to jail

--- clikToCloseMenu/scripts/clickToCloseMenu/player.lua
+++ clikToCloseMenu/scripts/clickToCloseMenu/player.lua
@@ -1,11 +1,11 @@
 local ui = require('openmw.ui')
 local UI = require('openmw.interfaces').UI
 
-
 local selfObject = self
 
 local function onMouseButtonPress(button)
-   if button == 3 then
+   local currentMode = UI.getMode()
+   if button == 3 and currentMode ~= "Dialogue" then
       UI.setMode()
    end      
 end
