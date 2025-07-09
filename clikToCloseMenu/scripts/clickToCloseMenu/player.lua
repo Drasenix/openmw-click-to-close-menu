19f89e73fb997c1f373cca3519b5618d8db21cc1
Drasenix <adowns@hotmail.fr>
2025-07-10 01:20:43 +0200

    adding settings to set the click used to close menu

--- clikToCloseMenu/scripts/clickToCloseMenu/player.lua
+++ clikToCloseMenu/scripts/clickToCloseMenu/player.lua
@@ -1,11 +1,26 @@
-local ui = require('openmw.ui')
+local core = require('openmw.core')
 local UI = require('openmw.interfaces').UI
+local configPlayer = require('scripts.clickToCloseMenu.config.player')
+local l10n = core.l10n('clickToCloseMenu')
+
 
 local selfObject = self
-
 local function onMouseButtonPress(button)
+   local clickNameForPreparingWeapon = l10n(configPlayer.options.s_Click)
+   local clickIdEquivalent
+   
+   if clickNameForPreparingWeapon == "Left" then
+      clickIdEquivalent = 1
+   elseif clickNameForPreparingWeapon == "Middle" then
+      clickIdEquivalent = 2
+   elseif clickNameForPreparingWeapon == "Right" then
+      clickIdEquivalent = 3
+   else
+      return
+   end
+
    local currentMode = UI.getMode()
-   if button == 3 and currentMode ~= "Dialogue" then
+   if button == clickIdEquivalent and currentMode ~= "Dialogue" then
       UI.setMode()
    end      
 end
