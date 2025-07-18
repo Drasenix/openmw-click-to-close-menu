573526d1da7d1e6989eee9fc7927dbd891383fa7
Drasenix <adowns@hotmail.fr>
2025-07-18 18:05:18 +0200

    fix missing import

--- clikToCloseMenu/scripts/clickToCloseMenu/player.lua
+++ clikToCloseMenu/scripts/clickToCloseMenu/player.lua
@@ -1,4 +1,5 @@
 local core = require('openmw.core')
+local self = require('openmw.self')
 local UI = require('openmw.interfaces').UI
 local configPlayer = require('scripts.clickToCloseMenu.config.player')
 local l10n = core.l10n('clickToCloseMenu')
